4f2d4cfdbdc90a036adf063ba8f35b51c49f627e
Volodymyr7711 <volodumurbodnarchuk70@gmail.com>
2021-03-01 16:10:59 +0200

    Initial Commit

---
+++ TheMovieApp/TheMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieAppApp.swift
+//  TheMovieApp
+//
+//  Created by Volodymyr Mendyk on 01.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
